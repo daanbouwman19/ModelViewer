b62a4a818fa92418ad92c0dc9de8a55170a15c6c
Daan Bouwman <daanbouwman19@gmail.com>
2025-12-10 00:18:50 +0000

    chore(devcontainer): remove unused settings and update postStartCommand for improved startup chore(config): enable polling for file watching in Vite configuration

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -38,6 +38,10 @@ export default defineConfig({
       host: '0.0.0.0',
       port: 5173,
       strictPort: true,
+      watch: {
+        usePolling: true,
+        interval: 100,
+      },
     },
     build: {
       sourcemap: false,
