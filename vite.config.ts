f373569fc6c4d592f04d4772b134348be7374cfd
daanbouwman19 <51409928+daanbouwman19@users.noreply.github.com>
2026-01-02 21:39:37 +0100

    build: Increase chunk size warning limit and configure manual chunkin… (#296)
    * build: Increase chunk size warning limit and configure manual chunking for vendor dependencies.
    
    * build: Add dedicated `vue` chunk and refine `three` chunking condition.

--- vite.config.ts
+++ vite.config.ts
@@ -72,9 +72,23 @@ export default defineConfig(({ mode }) => {
       target: 'es2020',
       sourcemap: mode === 'production' ? 'hidden' : true,
       outDir: 'dist/client',
+      chunkSizeWarningLimit: 1000,
       rollupOptions: {
         input: {
           index: resolve(__dirname, 'index.html'),
+        },
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              if (id.includes('node_modules/three/')) {
+                return 'three';
+              }
+              if (id.includes('node_modules/vue')) {
+                return 'vue';
+              }
+              return 'vendor';
+            }
+          },
         },
       },
     },
