3278fba77300dd064e2ec245bbcb567aa3eedd32
Daan Bouwman <daanbouwman19@gmail.com>
2026-01-20 20:35:07 +0100

    fixed tests

--- tests/renderer/components/AlbumsList.a11y.test.ts
+++ tests/renderer/components/AlbumsList.a11y.test.ts
@@ -58,6 +58,7 @@ describe('AlbumsList Accessibility', () => {
       albumsSelectedForSlideshow: {},
       smartPlaylists: [],
       historyMedia: [],
+      mediaDirectories: [],
     });
 
     mockPlayerState = reactive({
