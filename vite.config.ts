5b3173982e61e1e7143f49aebd27e17f184c976f
Daan Bouwman <daanbouwman19@gmail.com>
2026-01-30 10:18:58 +0100

    🔧 Update dependencies in package-lock.json and replace 'electron' with 'hls.js' in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig(({ mode }) => {
             'cors',
             'ffmpeg-static',
             'dotenv',
-            'electron',
+            'hls.js',
           ],
         },
         minify: 'terser',
